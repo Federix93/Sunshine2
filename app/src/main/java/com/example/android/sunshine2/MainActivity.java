9c767fef84fb33f515528501e6a6693fbac67af1
Federico <federico.rizzo93@gmail.com>
2016-09-12 09:43:15 +0200

    New fragment in Main Activity
    Add fragment in MainActivity and its layout

--- app/src/main/java/com/example/android/sunshine2/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine2/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.sunshine2;
 
+import android.app.Fragment;
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static class PlaceHolderFragment extends Fragment{
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View root = inflater.inflate(R.layout.fragment_main, container);
+            return root;
+        }
     }
 }
